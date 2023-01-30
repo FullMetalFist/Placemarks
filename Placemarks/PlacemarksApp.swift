c27eeb488dbdb2ec0180833b34f50ecfb3677f67
FullMetalFist <m.vilabrera@gmail.com>
2023-01-30 10:47:07 -0500

    initial commit

---
+++ Placemarks/PlacemarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacemarksApp.swift
+//  Placemarks
+//
+//  Created by Michael Vilabrera on 1/30/23.
+//
+
+import SwiftUI
+
+@main       // main app entry point
+struct PlacemarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
