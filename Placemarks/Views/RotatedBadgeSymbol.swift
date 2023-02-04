aed7adff148c5f735705e3410dae63c2e727be50
FullMetalFist <m.vilabrera@gmail.com>
2023-02-04 11:12:21 -0500

    Drawing Paths and Shapes: Draw the Badge Symbol

---
+++ Placemarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Placemarks
+//
+//  Created by Michael Vilabrera on 2/4/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol().padding(-60).rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 0))
+    }
+}
